f401f996b86dc8103d9df12b96651064549b5e84
darren277 <darren277@yahoo.com>
2025-01-02 03:21:34 -0800

    Added routing and two components for displaying patients and single patient details.

---
+++ src/components/PatientList.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+import api from '../services/api';
+
+const PatientList = () => {
+    const [patients, setPatients] = useState([]);
+
+    useEffect(() => {
+        //api.get('/patients')
+        api.get('http://127.0.0.1:5000/patients')
+            .then(response => {
+                console.log(response.data)
+                setPatients(response.data)
+            })
+            .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <div>
+            {/* Render patient list: patients = [{"first_name": "John", "last_name": "Doe", "age": 45, "history": john_doe_history}, {"first_name": "Jane", "last_name": "Doe", "age": 35, "history": jane_doe_history}] */}
+            <h1>Patient List</h1>
+            <ul>
+                {patients && patients.map((patient, index) => (
+                    <li key={index}><Link to={`/patients/${patient.id}`}>{patient.first_name} {patient.last_name}, {patient.age}</Link></li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default PatientList;
