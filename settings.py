3ada5a91712a30cae49d59922b7b8e1e9076c286
darren277 <darren277@yahoo.com>
2025-01-11 08:11:13 -0800

    Environment variable configuration file.

---
+++ settings.py
@@ -0,0 +1,36 @@
+""""""
+import os
+from os.path import join, dirname
+
+from dotenv import load_dotenv
+
+from logger import Logger
+
+logger = Logger()
+
+logger.info = print
+
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+
+SURREALDB_NAMESPACE = os.environ.get("SURREALDB_NAMESPACE")
+SURREALDB_DATABASE = os.environ.get("SURREALDB_DATABASE")
+SURREALDB_URL = os.environ.get("SURREALDB_URL")
+SURREALDB_USER = os.environ.get("SURREALDB_USER")
+SURREALDB_PASS = os.environ.get("SURREALDB_PASS")
+
+print("SUREALDB_NAMESPACE:", SURREALDB_NAMESPACE)
+print("SURREALDB_DATABASE:", SURREALDB_DATABASE)
+print("SURREALDB_URL:", SURREALDB_URL)
+print("SURREALDB_USER:", SURREALDB_USER)
+print("SURREALDB_PASS:", SURREALDB_PASS)
+
+PORT = os.environ.get('PORT', 5000)
+DEBUG = os.environ.get('DEBUG', True)
+HOST = os.environ.get('HOST', '0.0.0.0')
+
+print("PORT:", PORT)
+print("DEBUG:", DEBUG)
+print("HOST:", HOST)
