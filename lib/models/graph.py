cc0243b8587259a032b2495e992594fd82639969
darren277 <darren277@yahoo.com>
2025-04-07 04:52:50 -0700

    Testing connection and simple node creation.

--- lib/models/graph.py
+++ lib/models/graph.py
@@ -1,4 +1,6 @@
 """"""
+from lib.db.surreal import DbController
+from settings import SURREALDB_URL, SURREALDB_NAMESPACE, SURREALDB_USER, SURREALDB_PASS
 
 '''
 [AMT-011]: Graph Schema for SurrealDB
@@ -21,3 +23,12 @@ Edges:
 
 That will be our starting point at least and we can expand on it later.
 '''
+
+db = DbController(url=SURREALDB_URL, namespace=SURREALDB_NAMESPACE, database='graph', user=SURREALDB_USER, password=SURREALDB_PASS)
+db.connect()
+
+def create_node(node_type: str, node_id: str, node_name: str, **fields):
+    db.create(f'{node_type}:{node_id}', dict(name=node_name, **fields))
+
+create_node('symptom', 'loss_of_appetite', 'Loss of appetite')
+create_node('symptom', 'fatigue', 'Fatigue')
