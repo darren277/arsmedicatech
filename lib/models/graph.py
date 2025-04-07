a956ff708ea506bac6ce42f801d5cba5f0de8c4d
darren277 <darren277@yahoo.com>
2025-04-07 04:54:21 -0700

    Creating a few more sample nodes.

--- lib/models/graph.py
+++ lib/models/graph.py
@@ -27,8 +27,38 @@ That will be our starting point at least and we can expand on it later.
 db = DbController(url=SURREALDB_URL, namespace=SURREALDB_NAMESPACE, database='graph', user=SURREALDB_USER, password=SURREALDB_PASS)
 db.connect()
 
+
+
+
+
+
 def create_node(node_type: str, node_id: str, node_name: str, **fields):
     db.create(f'{node_type}:{node_id}', dict(name=node_name, **fields))
 
+
+
+
+# -- Create some symptoms
+# CREATE symptom:loss_of_appetite SET name = "Loss of appetite";
+# CREATE symptom:fatigue          SET name = "Fatigue";
+
 create_node('symptom', 'loss_of_appetite', 'Loss of appetite')
 create_node('symptom', 'fatigue', 'Fatigue')
+
+
+# -- Create some diagnoses
+# CREATE diagnosis:depression SET name = "Depression";
+# CREATE diagnosis:flu        SET name = "Influenza (Flu)";
+
+create_node('diagnosis', 'depression', 'Depression')
+create_node('diagnosis', 'flu', 'Influenza (Flu)')
+
+
+# -- Create some medications
+# CREATE medication:prozac    SET name = "Prozac";
+# CREATE medication:ibuprofen SET name = "Ibuprofen";
+# CREATE medication:warfarin  SET name = "Warfarin";
+
+create_node('medication', 'prozac', 'Prozac')
+create_node('medication', 'ibuprofen', 'Ibuprofen')
+create_node('medication', 'warfarin', 'Warfarin')
