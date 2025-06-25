40e83471c4181d6ff3d8d483f55c3c266bc5a34f
darren277 <darren277@yahoo.com>
2025-06-25 12:28:04 -0700

    New ProfilePanel component.

---
+++ src/components/ProfilePanel.jsx
@@ -0,0 +1,14 @@
+export const ProfilePanel = ({ user, onLogout }) => {
+    return (
+        <div className="profile-section">
+             <div className="customize-profile">
+                <span>Customize Profile</span>
+                <ChevronDownIcon />
+            </div>
+            <div className="user-profile">
+                <span>Hello<br/><b>Dr. Carvolth</b></span>
+                <div className="user-avatar"></div>
+            </div>
+        </div>
+    );
+}
