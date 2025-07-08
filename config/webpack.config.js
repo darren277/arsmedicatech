52bcf4ebd566dd9f098edd0c8b5e4e47910e26b9
darren277 <darren277@yahoo.com>
2025-07-08 12:48:55 -0700

    Configuration file adjustments.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,17 +1,22 @@
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PORT = process.env.PORT || 80;
 
 module.exports = {
-  entry: path.resolve(__dirname, "./src/index.tsx"),
+  entry: path.resolve(__dirname, '../src/index.tsx'),
   module: {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            configFile: path.resolve(__dirname, './babel.config.js'), // 👈 explicit path
+          },
+        },
       },
       {
         test: /\.css$/,
@@ -19,30 +24,41 @@ module.exports = {
         use: [
           { loader: 'style-loader' },
           { loader: 'css-loader' },
-          { loader: 'postcss-loader' },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                config: path.resolve(__dirname, './postcss.config.js'), // 👈 explicit path
+              },
+            },
+          },
         ],
       },
     ],
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx", ".ts", ".tsx", ".css"],
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx', '.css'],
   },
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "bundle.js",
-    publicPath: '/'
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(), 
-    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
     new webpack.DefinePlugin({
-      'process.env.API_URL': JSON.stringify(process.env.API_URL || 'http://127.0.0.1:3123'),
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
-    })
+      'process.env.API_URL': JSON.stringify(
+        process.env.API_URL || 'http://127.0.0.1:3123'
+      ),
+      'process.env.NODE_ENV': JSON.stringify(
+        process.env.NODE_ENV || 'development'
+      ),
+    }),
   ],
   devServer: {
     allowedHosts: 'all',
-    static: path.resolve(__dirname, "./public"),
+    static: path.resolve(__dirname, './public'),
     hot: true,
     port: PORT,
     historyApiFallback: true,
