76ca4d47c8a203e0c831f1fa3e77c6916f6cff94
darren277 <darren277@yahoo.com>
2025-07-12 07:57:54 -0700

    Small fixes to App file and some code formatting.

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,16 @@ import App from './App';
 import './index.css';
 // import * as serviceWorker from './serviceWorker';
 
-import * as Sentry from "@sentry/react";
+import * as Sentry from '@sentry/react';
 
 import { SENTRY_DSN } from './env_vars';
 
 Sentry.init({
-    dsn: SENTRY_DSN,
-    // Setting this option to true will send default PII data to Sentry.
-    // For example, automatic IP address collection on events
-    sendDefaultPii: true
+  dsn: SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
 });
-
-const container = document.getElementById(“app”);
-const root = createRoot(container);
-root.render(<App />);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // ReactDOM.render(<h3>Test</h3>, document.getElementById('root'));
