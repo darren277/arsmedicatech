db824b35a2067fc4cdc8bc030ce698012d252b52
darren277 <darren277@yahoo.com>
2025-07-15 06:42:29 -0700

    Module name wrapper for D3.

--- config/jest.config.js
+++ config/jest.config.js
@@ -3,6 +3,8 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
+    '^d3$': '<rootDir>/__mocks__/fileMock.js',
+    '^d3-.*$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
