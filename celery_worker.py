26b4f2b8cdb36fd52cbb76d85cf90dccb4218931
darren277 <darren277@yahoo.com>
2025-07-16 03:16:49 -0700

    Celery worker.

---
+++ celery_worker.py
@@ -0,0 +1,22 @@
+import os
+
+from celery import Celery
+
+# You can set these in your .env or settings.py
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
+
+
+celery_app = Celery(
+    "arsmedicatech",
+    broker=CELERY_BROKER_URL,
+    backend=CELERY_RESULT_BACKEND,
+)
+
+celery_app.conf.update(
+    task_serializer="json",
+    result_serializer="json",
+    accept_content=["json"],
+    timezone="UTC",
+    enable_utc=True,
+) 
