3e2945878b241b69d40e31d1f11b766196cc2de2
darren277 <darren277@yahoo.com>
2025-07-17 10:58:58 -0700

    Changed LiveKit server URL protocol.

--- src/env_vars.ts
+++ src/env_vars.ts
@@ -5,7 +5,6 @@ const SENTRY_DSN =
   'https://aec01cd03a9a408398ec656d4e7f5ddb@o4509655831412736.ingest.us.sentry.io/4509655843799040';
 
 const LIVE_KIT_TOKEN_URL = 'http://localhost:5001';
-const LIVE_KIT_SERVER_URL = 'http://localhost:7880';
+const LIVE_KIT_SERVER_URL = 'ws://localhost:7880';
 
 export { API_URL, LIVE_KIT_SERVER_URL, LIVE_KIT_TOKEN_URL, SENTRY_DSN };
-
