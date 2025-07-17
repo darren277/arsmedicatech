397a005467212f4661538da34534ceac01b4fc27
darren277 <darren277@yahoo.com>
2025-07-17 06:32:18 -0700

    LIVE_KIT_URL added to env_vars.

--- src/env_vars.ts
+++ src/env_vars.ts
@@ -4,4 +4,7 @@ const API_URL = process.env.API_URL || 'http://127.0.0.1:3123';
 const SENTRY_DSN =
   'https://aec01cd03a9a408398ec656d4e7f5ddb@o4509655831412736.ingest.us.sentry.io/4509655843799040';
 
-export { API_URL, SENTRY_DSN };
+const LIVE_KIT_URL = 'http://localhost:5001';
+
+export { API_URL, LIVE_KIT_URL, SENTRY_DSN };
+
